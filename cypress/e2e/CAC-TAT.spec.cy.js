53bc1166ed4cc6abe7c779d76212aead9002bef0
sidjp <sidjp05@gmail.com>
2023-03-29 11:44:18 -0400

    terminado aula30

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -1,103 +1,115 @@
-
 ///<reference types="Cypress" />
-describe('Central de Atendimento ao Cliente TAT', function () {
+describe("Central de Atendimento ao Cliente TAT", function () {
   beforeEach(function () {
+    cy.visit("./src/index.html");
+  });
 
-    cy.visit('./src/index.html')
-  })
+  it("verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
 
-  it('verifica o título da aplicação', function () {
+  it("preenche os campos obrigatórios e envia o formulário", function () {
+    cy.comandoPreencherCamposEnvia();
+    cy.get(".success").should("be.visible");
+  });
+  it("exibe mensagem de erro ao submeter o formulário com o email no formato inválido", function () {
+    cy.comandoVerificaEmail();
+    cy.get(".error").should("be.visible");
+  });
+  it("campo telefone continua vazio quando preenchido com valor não-numérico", function () {
+    cy.get("#phone").type("abcdefghij").should("have.value", "");
+  });
 
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  })
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio", function () {
+    cy.get("#firstName").type("sidney");
 
-  it('preenche os campos obrigatórios e envia o formulário', function () {
-    
-    cy.comandoPreencherCamposEnvia()
-    cy.get('.success')
-      .should('be.visible')
-  })
-  it('exibe mensagem de erro ao submeter o formulário com o email no formato inválido', function () {
+    cy.get("#lastName").type("mori");
 
-    cy.comandoVerificaEmail()
-    cy.get('.error')
-      .should('be.visible')
-  })
-  it('campo telefone continua vazio quando preenchido com valor não-numérico', function () {
+    cy.get("#email").type("sidjp@gmail.com");
 
-    cy.get('#phone')
-      .type('abcdefghij')
-      .should('have.value', '')
+    cy.get("#phone-checkbox").check();
 
-  })
+    cy.get("#open-text-area").type("test");
 
-  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio', function () {
+    cy.get('button[type="submit"]').click();
 
-    cy.get('#firstName')
-      .type('sidney')
+    cy.get(".error").should("be.visible");
+  });
 
-    cy.get('#lastName')
-      .type('mori')
-
-    cy.get('#email')
-      .type('sidjp@gmail.com')
-
-    cy.get('#phone-checkbox')
-      .click()
-
-    cy.get('#open-text-area')
-      .type('test')
-
-    cy.get('button[type="submit"]')
-      .click()
-
-    cy.get('.error')
-      .should('be.visible')
-  })
-
-  it('preenche e limpa os campos nome , sobrenome, email e telefone ', function () {
-
-    cy.get('#firstName')
-      .type('sidney')
-      .should('have.value', 'sidney')
+  it("preenche e limpa os campos nome , sobrenome, email e telefone ", function () {
+    cy.get("#firstName")
+      .type("sidney")
+      .should("have.value", "sidney")
       .clear()
-      .should('have.value', '')
+      .should("have.value", "");
 
-    cy.get('#lastName')
-      .type('mori')
-      .should('have.value', 'mori')
+    cy.get("#lastName")
+      .type("mori")
+      .should("have.value", "mori")
       .clear()
-      .should('have.value', '')
+      .should("have.value", "");
 
-    cy.get('#email')
-      .type('sidjp@gmail.com')
-      .should('have.value', 'sidjp@gmail.com')
+    cy.get("#email")
+      .type("sidjp@gmail.com")
+      .should("have.value", "sidjp@gmail.com")
       .clear()
-      .should('have.value', '')
+      .should("have.value", "");
 
-    cy.get('#open-text-area')
-      .type('test')
-      .should('have.value', 'test')
+    cy.get("#open-text-area")
+      .type("test")
+      .should("have.value", "test")
       .clear()
-      .should('have.value', '')
+      .should("have.value", "");
 
-    cy.get('#phone')
-      .type('123456789')
-      .should('have.value', '123456789')
+    cy.get("#phone")
+      .type("123456789")
+      .should("have.value", "123456789")
       .clear()
-      .should('have.value', '')
+      .should("have.value", "");
+  });
 
-  })
+  it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", function () {
+    cy.get('button[type="submit"]').click();
 
-  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+    cy.get(".error").should("be.visible");
+  });
 
-    cy.get('button[type="submit"]')
-      .click()
-
-    cy.get('.error')
-      .should('be.visible')
-
-  })
-
-  
-})
+  it("selecione um produto Youtube por seu texto", function () {
+    cy.get("#product").select("YouTube").should("have.value", "youtube");
+  });
+  it("seleciona um produto Mentoria por seu valor value", function () {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
+  it("selecione um produto Blog por seu indice", function () {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
+  it("marca um tipo de atendimento feedback ", function () {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should("have.value", "feedback");
+  });
+  it("marca cada tipo de atendimento ", function () {
+    cy.get('input[type="radio"]')
+      .should("have.length", 3)
+      .each(function ($radio) {
+        cy.wrap($radio).check();
+        cy.wrap($radio).should("be.checked");
+      });
+  });
+  it("marcar ambos checkboxes,depois desmarca o ultimo ", function () {
+    cy.get('input[type="checkbox"]')
+    .check()
+    .should("be.checked")
+    .last()
+    .uncheck()
+    .should("not.be.checked");
+  });
+  it.only("marcar ambos checkboxes,depois desmarca o ultimo ", function () {
+    cy.get('input[type="file"]#file-upload')
+    .should("not.have.value")
+    .selectFile('./cypress/fixtures/example.json')
+    .should(function($input){
+      expect($input[0].files[0].name).to.equal('example.json')
+    });
+  });
+});
