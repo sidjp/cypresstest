6c130e328211cbac9ac449deb0e04135920945d9
sidjp <sidjp05@gmail.com>
2023-03-31 08:50:36 -0400

    modificado

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -112,7 +112,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
       expect($input[0].files[0].name).to.equal('example.json')
     });
   });
-  it.only("selecione um arquivo simulando um drag-and-drop ", function () {
+  it("selecione um arquivo simulando um drag-and-drop ", function () {
     cy.get('input[type="file"]')
     .should("not.have.value")
     .selectFile('./cypress/fixtures/example.json', {actio: 'drag-drop'})
@@ -126,10 +126,10 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     .selectFile('@sampleFile')
     .should("not.have.value")
   });
-  it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
     cy.get('#privacy a').should('have.attr', 'target', '_blank');
   })
-  it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
     cy.get('#privacy a')
     .invoke('removeAttr', 'target')
     .click()
